dc65511002779d67849f05b904bb6b013e9373fc
Petros Apostolou <petros@metagramhealth.com>
2025-08-17 20:50:43 -0400

    Downgrade Node version to fix crypto.hash

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-/*import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
-*/
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/CodeIT/',    // Use './' for relative paths, works well with GitHub Pages
+  base: '/CodeIT/',
   plugins: [vue()],
 })
