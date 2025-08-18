84293aa015e074a2e5dc39469597e3222f3203fe
Petros Apostolou <petros@metagramhealth.com>
2025-08-17 22:17:21 -0400

    update deployment-8-17-2025-4

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 // vite.config.js
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
@@ -8,3 +8,18 @@ export default defineConfig({
     exclude: ['react-native']
   }
 })
+*/
+
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// Detect base automatically for GitHub Pages
+const isGithubPages = process.env.GITHUB_REPOSITORY
+const repoName = isGithubPages ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/'
+
+export default defineConfig({
+  base: repoName,
+  plugins: [vue()],
+})
+
