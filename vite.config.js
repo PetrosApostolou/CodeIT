9fe1f7d19d4c66a8a553560eb30b3c6307f85b3d
Petros Apostolou <petros@metagramhealth.com>
2025-08-17 21:46:53 -0400

    update main again

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/CodeIT/',
   plugins: [vue()],
+  optimizeDeps: {
+    exclude: ['react-native']
+  }
 })
